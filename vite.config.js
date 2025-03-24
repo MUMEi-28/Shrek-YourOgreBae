59cddafe93d45d57590b4b6d727c62060f7801ee
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-24 17:16:41 +0800

    waw unli push dahil dito para lang madeploy e
    -_-

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+/* import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -9,4 +9,17 @@ export default defineConfig({
   tailwindcss(),
   ],
   base: "/Shrek-YourOgreBae",
+})
+
+ */
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  tailwindcss(),
+  ],
 })
