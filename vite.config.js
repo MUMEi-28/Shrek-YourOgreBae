ceee3b5eeaa95294fbb822ae3ec44cc74a606616
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-24 13:59:08 +0800

    deploy ayaw gumana ng env

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import dotenv from 'dotenv'
 
 
 // https://vite.dev/config/
@@ -8,5 +9,8 @@ export default defineConfig({
   plugins: [react(),
   tailwindcss(),
   ],
-  base: "/Shrek-YourOgreBae"
+  base: "/Shrek-YourOgreBae",
+  define: {
+    'process.env.VITE_HF_ACCESS_TOKEN': JSON.stringify(import.meta.env.VITE_HF_ACCESS_TOKEN),
+  }
 })
